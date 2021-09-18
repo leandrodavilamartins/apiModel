2e5d9d4cb02964fd495e349e9e42cac612e5e7d8
Leandro <leandrodavilamartins@gmail.com>
2021-09-18 17:46:37 -0300

    PUT and DELETE requests implementation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-app.use(express.json());
+app.use(express.json()); // middleware que permite acessar objetos json no corpo da requisição 
 
 mongoose.connect('mongodb+srv://granjamatinha:matinha@cluster0.te0mg.mongodb.net/Estoque6?retryWrites=true&w=majority')
 .then( response => {
@@ -12,23 +12,23 @@ mongoose.connect('mongodb+srv://granjamatinha:matinha@cluster0.te0mg.mongodb.net
     console.error(err.message);
 })
 
-const dataSchema = new mongoose.Schema({
+const dataSchema = new mongoose.Schema({ // cria schema para o model 
     name: String,
     quantity : Number
 })
 
-const dataModel = mongoose.model('items', dataSchema);
+const dataModel = mongoose.model('items', dataSchema); // cria model 
 
 
-app.get('/', (req,res) => {
+app.get('/', (req,res) => { // home page 
     res.send('Homepage da api');
 });
 
-app.get('/about', (req,res) => {
+app.get('/about', (req,res) => {  // página about 
     res.send('About page');
 })
 
-app.get('/estoque', (req,res) => {
+app.get('/estoque', (req,res) => { // retorna estoque 
     dataModel.find({})
     .then( response => {
         res.json(response);
@@ -38,7 +38,7 @@ app.get('/estoque', (req,res) => {
     })
 })
 
-app.post('/estoque', (req,res) => {
+app.post('/estoque', (req,res) => { // adiciona item ao estoque 
     const data = req.body;
     const modelInstance = new dataModel({
         name : data.name,
@@ -55,7 +55,7 @@ app.post('/estoque', (req,res) => {
     })
 })
 
-app.get('/estoque/:id', async (req,res) => {
+app.get('/estoque/:id', async (req,res) => { // retorna item do estoque 
     const index = req.params.id;
     const items = await readDatabase();
     const filteredItems = items.filter( item => {
@@ -64,11 +64,37 @@ app.get('/estoque/:id', async (req,res) => {
     res.send(filteredItems);
 })
 
+app.put('/estoque/:id', (req,res) => { // atualiza item de estoque 
+    const index = req.params.id;
+    const data = req.body;
+    dataModel.findByIdAndUpdate(index, {name: data.name, quantity: data.quantity})
+    .then(() => { 
+        console.log('Atualização realizada com sucesso !');
+        res.send('Atualização de item realizada com sucesso !');
+    })
+    .catch( err => {
+        console.log('Não foi possível atualizar item !');
+    })
+})
+
+app.delete('/estoque/:id', (req,res) => { // deleta item de estoque 
+    const index = req.params.id;
+    dataModel.findByIdAndDelete(index)
+    .then( () => {
+        console.log("Item removido com sucesso !");
+        res.send("Item removido com sucesso !");
+    })
+    .catch( err => {
+        res.send("Não foi possível remover item !");
+    })
+})
+
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
 
 async function readDatabase(){
-    const result = await dataModel.find({})
+    var result = [];
+    result = await dataModel.find({})
     return result;
 }
