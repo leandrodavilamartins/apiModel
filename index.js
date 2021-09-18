9b6d54a0d49eb6bd6e1f7a7645d8e56ae089ef39
Leandro <leandrodavilamartins@gmail.com>
2021-09-17 23:13:20 -0300

    GET /estoque route handler implementation

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://granjamatinha:matinha@cluster0.te0mg.mongodb.net/mySecondDatabase?retryWrites=true&w=majority')
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://granjamatinha:matinha@cluster0.te0mg.mongodb.net/Estoque6?retryWrites=true&w=majority')
 .then( response => {
     console.log('Connected to mongodb database ...');
 })
@@ -10,9 +12,32 @@ mongoose.connect('mongodb+srv://granjamatinha:matinha@cluster0.te0mg.mongodb.net
     console.error(err.message);
 })
 
+const dataSchema = new mongoose.Schema({
+    name: String,
+    quantity : Number
+})
+
+const dataModel = mongoose.model('items', dataSchema);
+
+
 app.get('/', (req,res) => {
     res.send('Homepage da api');
 });
+
+app.get('/about', (req,res) => {
+    res.send('About page');
+})
+
+app.get('/estoque', (req,res) => {
+    dataModel.find({})
+    .then( response => {
+        res.json(response);
+    })
+    .catch( err => {
+        console.error(err);
+    })
+})
+
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
